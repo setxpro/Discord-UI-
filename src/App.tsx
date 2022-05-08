25c6a7ad18f7a14006a719de219dfe6796459d7b
Patrick <patrickpqdt87289@gmail.com>
2022-05-07 22:52:33 -0300

    persisted-state-theme-in-localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Main from './Components/Main';
 
+import usePersistedState from './utils/usePersistedState';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+import light from './styles/theme/light';
+import dark from './styles/theme/dark';
+import Header from './Components/Header';
+
 const App: React.FC = () => {
+
+  // initial State
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+  const toggleTheme = () => setTheme(theme.title === 'light' ? dark : light);
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <Header toggleTheme={toggleTheme}/>
       <Main/>
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Layout/styles.ts
+++ src/Components/Layout/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    
+    width: 100%;
+    height: 100vh; // only view
+    transition: all 1s ease;
+    background: ${props => props.theme.colors.secondary};
+    color: ${props => props.theme.colors.senary};
 `;
